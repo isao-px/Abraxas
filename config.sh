e6bce69db75e4b8c65cbf940f5c5e7b8d3989162
Isao Perregaux <160398038+isao-px@users.noreply.github.com>
2026-05-20 09:10:57 +0200

    Refactor config.sh to use absolute paths

--- config.sh
+++ config.sh
@@ -36,17 +36,16 @@ raspiconfig
 # IMU
 echo "Installing ICM20948 IMU"
 apt -y install git
-cd ~ || exit
+cd /home/"$user"/ || exit
 git clone https://github.com/pimoroni/icm20948-python
-cd icm20948-python || exit
+cd /home/"$user"/icm20948-python || exit
 ./install.sh -n
-cd ~ || exit
 pip3 install icm20948 --break-system-packages
 echo "ICM20948 IMU installed successfully"
 
 # GPS
 echo "Installing GPS"
-cd ~ || exit
+cd /home/"$user"/ || exit
 apt -y install --upgrade python3-setuptools
 apt -y install python3-venv
 python3 -m venv env --system-site-packages
@@ -55,42 +54,39 @@ pip3 install --upgrade adafruit-python-shell --break-system-packages
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
 sudo -E venv PATH="$PATH" python3 raspi-blinka.py
 pip3 install adafruit-circuitpython-gps --break-system-packages --root-user-action=ignore
-cd ~ || exit
 echo "GPS installed successfully"
 
 # Download code
 echo "Downloading code"
-cd ~ || exit
+cd /home/"$user"/ || exit
 git clone https://github.com/isao-px/abraxas.git
-mv abraxas/SYS/*.py .
-mv abraxas/config.sql config.sql
-rm -rf abraxas
-cd ~ || exit
+mv /home/"$user"/abraxas/SYS/*.py /home/"$user"/
+mv /home/"$user"/abraxas/config.sql /home/"$user"/config.sql
+rm -rf /home/"$user"/abraxas
 echo "Code downloaded successfully"
 
 # DB
 echo "Initializing database"
-cd ~ || exit
-if [ ! -f sys.db ]; then
-    touch sys.db
-    sqlite3 sys.db < config.sql
+cd /home/"$user"/ || exit
+if [ ! -f /home/"$user"/sys.db ]; then
+    touch /home/"$user"/sys.db
+    sqlite3 /home/"$user"/sys.db < config.sql
 else
     echo "Database already exists"
 fi
-cd ~ || exit
 echo "Database initialized successfully"
 
 # Configuration of cron
-chmod +x interface.py
+chmod +x /home/"$user"/interface.py
 crontab -u "$user" -l > /tmp/cron_config
 "@reboot python3 /home/$user/interface.py" >> /tmp/cron_config
 crontab -u "$user" /tmp/cron
 rm /tmp/cron_config
 
 # Cleanup
-cd ~ || exit
-rm config.sql
+cd /home/"$user"/ || exit
+rm /home/"$user"/config.sql
 echo "Configuration script completed at $(date +%Y-%m-%d_%H:%M:%S)"
-# rm config.sh
+rm /home/"$user"/config.sh
 echo "The Raspberry will reboot now"
 reboot
